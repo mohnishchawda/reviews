76c854260d3c1edb85ac86d8434ca25c8fe9c0c6
Mohnish Chawda <mr.m.chawda@gmail.com>
2024-10-07 17:20:30 +0100

    finalised api

--- my-app/app/lib/getReviews.js
+++ my-app/app/lib/getReviews.js
@@ -12,6 +12,24 @@ export const getReviews = async (country, id) => {
     .then((json_response) => {
       if (json_response["feed"]["entry"]) {
         const reviews = json_response["feed"]["entry"];
+        reviews.map((item) => {
+          item.date = item.updated.label;
+          item.rating = item["im:rating"].label;
+          item.version = item["im:version"].label;
+          item.title = item.title.label;
+          item.content = item.content.label;
+
+          delete item["im:version"];
+          delete item["im:rating"];
+          delete item.updated;
+          delete item.author;
+          delete item.id;
+          delete item.link;
+          delete item["im:contentType"];
+          delete item["im:voteSum"];
+          delete item["im:voteCount"];
+          return item;
+        });
         console.log(reviews);
       }
     });
